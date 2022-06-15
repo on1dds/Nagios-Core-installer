6629d7762fe2b6cac8485e831cbc4fdaeceaee49
on1dds <joachim.elen@belgacom.net>
2022-06-15 22:55:33 +0200

    Update 10-basics.sh

--- 10-basics.sh
+++ 10-basics.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo ============================================================================
+echo  basic setup
+echo ============================================================================
+
 #echo "Set hostname"
 #hostnamectl set-hostname monitoring.home.lan
 
